6c6c2007730a7382bf4565e6dff138e266b3dded
Marks-a <73535565+Marks-a@users.noreply.github.com>
2020-11-27 22:19:17 +0200

    Update react.js

--- react.js
+++ react.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,image, stylesheet,style } from 'react-native';
 
 const HelloWorldApp = () => {
   return (
@@ -11,6 +11,9 @@ const HelloWorldApp = () => {
       }}>
       <Text>Hello World from Group 10 group  </Text>
       <text>This is Ričards Vīdners Marks.Auzins and Kristaps Ciguzis first React Native application! </text>
+      <image source= {{ uri: "http://picsum.photos/200/300" }} style={{ 
+      width:300,
+      height: 300, }}/>
     </View>
   )
 }
