1c0c4dda37182d8ae7557d0445bec878921a87f3
Marks-a <73535565+Marks-a@users.noreply.github.com>
2020-11-27 22:07:26 +0200

    Create react.js

---
+++ react.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Hello World from Group 10 group  </Text>
+      <text>This is Ričards Vīdners Marks.Auzins and Kristaps Ciguzis first React Native application! </text>
+    </View>
+  )
+}
+export default HelloWorldApp;
